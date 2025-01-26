47aaaa194eb81075a8741322a483c28e45553a7f
Jorge Garcia <jorge.gar.per@gmail.com>
2025-01-26 02:25:52 +0100

    Min 3.53h Frontend SignUp, Login, Logout completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,18 @@ import App from './App.jsx'
 
 import { BrowserRouter } from 'react-router-dom'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Initialize the tanstack query client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { MdOutlineMail } from 'react-icons/md';
-import { RiLockPasswordLine } from 'react-icons/ri';
-import XSvg from "../../../components/svgs/X.jsx"
+import { MdOutlineMail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import XSvg from "../../../components/svgs/X.jsx";
+
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
+  });
+
+  const {
+    mutate: loginMutation,
+    isError,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: async ({ email, password }) => {
+      try {
+        const res = await fetch("/api/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        });
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Failed to create account");
+        if (data.error) throw new Error(data.error);
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+    onSuccess: () => {
+      toast.success("User logged successfully");
+    },
   });
 
   const handleInputChange = (e) => {
@@ -21,49 +54,56 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // TODO: Add login logic
+    loginMutation(formData);
   };
 
   return (
-    <div className='max-w-screen-xl mx-auto flex h-screen px-10'>
-      <div className='flex-1 hidden lg:flex items-center justify-center'>
-        <XSvg className='lg:w-2/3 fill-white' />
+    <div className="max-w-screen-xl mx-auto flex h-screen px-10">
+      <div className="flex-1 hidden lg:flex items-center justify-center">
+        <XSvg className="lg:w-2/3 fill-white" />
       </div>
-      <div className='flex-1 flex flex-col justify-center items-center'>
-        <form className='lg:w-2/3 mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-          <XSvg className='w-24 lg:hidden fill-white' />
-          <h1 className='text-4xl font-extrabold text-white'>Welcome back.</h1>
-          <label className='input input-bordered rounded flex items-center gap-2'>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <form
+          className="lg:w-2/3 mx-auto md:mx-20 flex gap-4 flex-col"
+          onSubmit={handleSubmit}
+        >
+          <XSvg className="w-24 lg:hidden fill-white" />
+          <h1 className="text-4xl font-extrabold text-white">Welcome back.</h1>
+          <label className="input input-bordered rounded flex items-center gap-2">
             <MdOutlineMail />
             <input
-              type='email'
-              className='grow'
-              placeholder='Email'
-              name='email'
+              type="email"
+              className="grow"
+              placeholder="Email"
+              name="email"
               onChange={handleInputChange}
               value={formData.email}
             />
           </label>
-          <label className='input input-bordered rounded flex items-center gap-2'>
+          <label className="input input-bordered rounded flex items-center gap-2">
             <RiLockPasswordLine />
             <input
-              type='password'
-              className='grow'
-              placeholder='Password'
-              name='password'
+              type="password"
+              className="grow"
+              placeholder="Password"
+              name="password"
               onChange={handleInputChange}
               value={formData.password}
             />
           </label>
-          <button type='submit' className='btn btn-primary rounded'>
-            Sign In
+          <button type="submit" className="btn btn-primary rounded">
+            {isPending ? "Loading..." : "Sign In"}
           </button>
+          {isError && <p className="text-red-500">{error.message}</p>}
         </form>
-        <div className='flex flex-col lg:w-2/3 gap-2 mt-4'>
-					<p className='text-white text-lg'>Dont have an account?</p>
-					<Link to='/signup'>
-						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
-					</Link>
-				</div>
+        <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
+          <p className="text-white text-lg">Dont have an account?</p>
+          <Link to="/signup">
+            <button className="btn rounded-full btn-primary text-white btn-outline w-full">
+              Sign up
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
